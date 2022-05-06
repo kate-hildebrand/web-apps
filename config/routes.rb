2a4f08ad200a7be4c6753783e275e7b26eb67005
kate-hildebrand <kate.hildebrand@kellogg.northwestern.edu>
2022-05-06 19:51:54 +0000

    midway

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  
+    resources "tacos"
+  #create a controller with the "rails generate controller tacos" 
+  # go to app-> views -> tacos after 
+
+  # we go back and repeat the process 
+    resources "dice"
+    # get an index error, solve by going to app -> views -> dice.
+    # create a new file named index.html.erb
 end
